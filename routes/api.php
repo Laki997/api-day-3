c9908607dbd1215ef8d676ec98fd9b6ef97b9130
Laki997 <laki.kalajdzic@gmail.com>
2021-06-03 22:01:56 +0200

    login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login',[LoginController::class,'authenticate']);
+
+Route::middleware('jwt')->get('/getUser',[LoginController::class,'getUser']);
